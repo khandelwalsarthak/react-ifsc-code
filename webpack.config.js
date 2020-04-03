4bbce568a9d7a44b700bea8a70f3bcee5ea6cd01
Sarthak Khandelwal <sarthak.khandelwal@paytm.com>
2020-04-03 13:11:52 +0530

    updated package json file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'index_bundle.js'
+    filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json']
